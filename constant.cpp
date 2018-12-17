251d136c6d81556be6cb66142ff341e5bb39465c
Arsh Singh <33452790+iosdev474@users.noreply.github.com>
2018-12-17 21:34:14 +0530

    Update for v1.36.6.9 (14.12.2018)
    Update for v1.36.6.9 (14.12.2018)

--- constant.cpp
+++ constant.cpp
@@ -1,14 +1,14 @@
 #include <windows.h>
-// for Executable version:Client v1.36.3.4 (05.05.2018)
+// for Executable version:Client v1.36.6.9 (14.12.2018)
 //base addresses
-const DWORD PlayerBase = 0xA9ADEC;
-const DWORD EntityBase = 0x4A77B6C;
-const DWORD ClientState = 0x57D844;
+const DWORD PlayerBase = 0xCB3694;		//dwLocalPlayer;
+const DWORD EntityBase = 0x4CC3564;		//dwEntityList;
+const DWORD ClientState = 0x58ACFC;		//dwClientState;
 //Offsets with client.dll and playerbase
-const DWORD CrossHairOffset = 0xB2A4;
-const DWORD PositionOffset = 0x134;
-const DWORD TeamOffset = 0xF0;
-const DWORD HealthOffset = 0xFC;
+const DWORD CrossHairOffset = 0xB390;	//m_iCrosshairId;
+const DWORD PositionOffset = 0x138;   //m_vecOrigin 
+const DWORD TeamOffset = 0xF4;			  //m_iTeamNum
+const DWORD HealthOffset = 0x100;     //m_iHealth 
 const DWORD MemoryIncrement = 0x10;
 //Offsets with engine.dll
-const DWORD ClientStateViewAngleOffset = 0x4D10;
+const DWORD ClientStateViewAngleOffset = 0x4D10;  //dwClientState_ViewAngles 
